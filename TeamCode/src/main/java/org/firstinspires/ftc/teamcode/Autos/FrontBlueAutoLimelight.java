cd28759e674909358838408125e03c0d3303caf8
Timothy Leedberg <timothyleedberg@icloud.com>
2026-01-28 16:21:40 -0500

    Added limelight auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/FrontBlueAutoLimelight.java
@@ -0,0 +1,309 @@
+package org.firstinspires.ftc.teamcode.Autos;
+
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+ * This OpMode illustrates the concept of driving a path based on encoder counts.
+ * The code is structured as a LinearOpMode
+ *
+ * The code REQUIRES that you DO have encoders on the wheels,
+ *   otherwise you would use: RobotAutoDriveByTime;
+ *
+ *  This code ALSO requires that the drive Motors have been configured such that a positive
+ *  power command moves them forward, and causes the encoders to count UP.
+ *
+ *   The desired path in this example is:
+ *   - Drive forward for 48 inches
+ *   - Spin right for 12 Inches
+ *   - Drive Backward for 24 inches
+ *   - Stop and close the claw.
+ *
+ *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
+ *  that performs the actual movement.
+ *  This method assumes that each movement is relative to the last stopping place.
+ *  There are other ways to perform encoder based moves, but this method is probably the simplest.
+ *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="front blue limelight", group="Robot")
+//@Disabled
+public class FrontBlueAutoLimelight extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private DcMotorEx leftDrive = null;
+    private DcMotorEx rightDrive = null;
+    private DcMotor intakeMotor = null;
+    private DcMotor channelMotor = null;
+    private DcMotor turretMotor = null;
+    private Servo leftFinger = null;
+    private Servo rightFinger = null;
+    private DcMotorEx shooterMotorA = null;
+    private DcMotorEx shooterMotorB = null;
+    private Limelight3A limeLight = null;
+
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    // Calculate the COUNTS_PER_INCH for your specific drive train.
+    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
+    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
+    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
+    // This is gearing DOWN for less speed and more torque.
+    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
+    static final double     COUNTS_PER_MOTOR_REV    = 560 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+
+
+    public static double P = 0.1;
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize the drive system variables.
+        leftDrive  = hardwareMap.get(DcMotorEx.class, "left_back_motor");
+        rightDrive = hardwareMap.get(DcMotorEx.class, "right_back_motor");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intake_motor");
+        channelMotor = hardwareMap.get(DcMotor.class, "channel_motor");
+        turretMotor = hardwareMap.get(DcMotor.class, "turret_motor");
+        leftFinger = hardwareMap.get(Servo.class, "left_finger");
+        rightFinger = hardwareMap.get(Servo.class, "right_finger");
+        shooterMotorA = hardwareMap.get(DcMotorEx.class, "shooter_motor_a");
+        shooterMotorB = hardwareMap.get(DcMotorEx.class, "shooter_motor_b");
+        limeLight = hardwareMap.get(Limelight3A.class, "Limelight2");
+
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        leftDrive.setDirection(DcMotorEx.Direction.FORWARD);
+        rightDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        intakeMotor.setDirection(DcMotor.Direction.REVERSE);
+        channelMotor.setDirection(DcMotor.Direction.REVERSE);
+        turretMotor.setDirection(DcMotor.Direction.REVERSE);
+        shooterMotorA.setDirection(DcMotorEx.Direction.FORWARD);
+        shooterMotorB.setDirection(DcMotorEx.Direction.REVERSE);
+
+
+        leftDrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        intakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        channelMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        shooterMotorA.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        shooterMotorB.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftDrive.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        intakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        channelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooterMotorA.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        shooterMotorB.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Starting at",  "%7d :%7d",
+                leftDrive.getCurrentPosition(),
+                rightDrive.getCurrentPosition());
+
+        telemetry.update();
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+
+        // Step through each leg of the path,
+        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        //turretMotor.setPower(-1);
+        //sleep(500);
+        //turretMotor.setPower(0);
+
+        servoDown();
+        intakeStart();
+        //encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        //encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED, -45, -45, 6.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+        intakeStop();
+
+        autoAim();
+
+        shooterAuto(2450,3, 4);
+        encoderDrive(TURN_SPEED, -4, 4, 4);
+        intakeStart();
+        encoderDrive(DRIVE_SPEED, 40, 40, 4);
+        encoderDrive(DRIVE_SPEED, -40, -40, 4);
+        encoderDrive(TURN_SPEED, 4.5, -4.5, 4);
+
+        //turretMotor.setPower(1);
+        //sleep(500);
+        //turretMotor.setPower(0);
+
+        intakeStop();
+        autoAim();
+        shooterAuto(2450,3, 4);
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);  // pause to display final telemetry message.
+    }
+
+    /*
+     *  Method to perform a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the OpMode running.
+     */
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        // Ensure that the OpMode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = leftDrive.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = rightDrive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            leftDrive.setTargetPosition(newLeftTarget);
+            rightDrive.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            leftDrive.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            rightDrive.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            //leftDrive.setPower(Math.abs(speed));
+            //rightDrive.setPower(Math.abs(speed));
+
+            PIDFCoefficients pidfCoefficientsL = new PIDFCoefficients(21, 0, 0, 14);
+            PIDFCoefficients pidfCoefficientsR = new PIDFCoefficients(16.5, 0, 0, 13);
+
+            leftDrive.setVelocity(1100);
+            rightDrive.setVelocity(1100);
+
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (leftDrive.isBusy() && rightDrive.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Running to",  " %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Currently at",  " at %7d :%7d",
+                        leftDrive.getCurrentPosition(), rightDrive.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            sleep(250);   // optional pause after each move.
+        }
+    }
+
+    public void shooterAuto(double rpm, long upTime, long fireTime){
+        //shooterMotorA.setVelocity((rpm/60)*28);
+        //shooterMotorB.setVelocity((rpm/60)*28);
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(300,0,0,15);
+        shooterMotorA.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        shooterMotorB.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        shooterMotorA.setVelocity((rpm/60)*28);
+        shooterMotorB.setVelocity((rpm/60)*28);
+
+        sleep(upTime*1000);
+        servoUp();
+        sleep(100);
+        channelMotor.setPower(0.5);
+        //intakeStart();
+        intakeMotor.setPower(1);
+        sleep(fireTime*1000);
+        servoDown();
+        channelMotor.setPower(0);
+        intakeStop();
+        shooterMotorA.setVelocity(0);
+        shooterMotorB.setVelocity(0);
+    }
+
+    public void intakeStart(){
+        intakeMotor.setPower(1);
+    }
+
+    public void intakeStop(){
+        intakeMotor.setPower(0);
+    }
+    public void servoDown(){
+        rightFinger.setPosition(0.11);
+        leftFinger.setPosition(0.42);
+    }
+
+    public void servoUp(){
+        rightFinger.setPosition(0.39);
+        leftFinger.setPosition(0.14);
+    }
+
+    public void autoAim() {
+        double error = turretAimCamera();
+        while (Math.abs(error) > 1){
+            error = turretAimCamera();
+        }
+    }
+
+    public double turretAimCamera() {
+        LLResult llResult = limeLight.getLatestResult();
+        if (llResult != null && llResult.isValid()){
+            double turret_error = -(P * llResult.getTx());
+            if ((Math.abs(llResult.getTx()) > 1) && llResult.isValid()){
+                turretMotor.setPower(turret_error);
+            }else{
+                turretMotor.setPower(0);
+            }
+            double dk = 32722.097;
+            double distance = Math.sqrt(dk/llResult.getTa());
+            telemetry.addData("I can see april tag", llResult.getBotposeTagCount());
+            telemetry.addData("Tx", llResult.getTx());
+            telemetry.addData("Ty", llResult.getTy());
+            telemetry.addData("Ta", llResult.getTa());
+            telemetry.addData("Distance from april tag is ", distance);
+
+            telemetry.update();
+
+            return turret_error;
+
+        }else{
+            turretMotor.setPower(0);
+            telemetry.addData("I can not see april tag :(", llResult.getBotposeTagCount());
+            telemetry.update();
+            return 0;
+        }
+    }
+
+
+}
+
